a6b4b1480ea86f4bf39b1e51345750a28c668532
TheMatrix2 <ermolovich.boris@gmail.com>
2023-11-19 21:58:08 +0300

    Made simplex class for maximization and minimization

--- Direct&DualProblem/main.py
+++ Direct&DualProblem/main.py
@@ -1,1 +1,20 @@
-import Simplex
+import numpy as np
+from Simplex import Simplex
+
+
+# входные данные
+c = np.array([2, 6, 7])
+A = np.array([[3, 1, 1],
+              [1, 2, 0],
+              [0, 0.5, 2]])
+b = np.array([3, 8, 1])
+
+# для прямой задачи
+print('\nDIRECT PROBLEM\n')
+simplex_direct = Simplex(A, b, c)
+simplex_direct.calculate()
+
+# для двойственной задачи
+print('\nDUAL PROBLEM\n')
+simplex_dual = Simplex(A.T, c.T, b.T, maximize=False)
+simplex_dual.calculate()
